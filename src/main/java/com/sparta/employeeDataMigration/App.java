bfdde2f104378d7cc41543fbfb4aa435548abe2b
Liam <liam-richo12@hotmail.co.uk>
2023-04-11 15:26:47 +0100

    CSV readFile() reads file
    csv readfile reads file

--- src/main/java/com/sparta/employeeDataMigration/App.java
+++ src/main/java/com/sparta/employeeDataMigration/App.java
@@ -4,6 +4,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        //testing
+
+        for (String string : CSVFil)
     }
 }
--- src/main/java/com/sparta/employeeDataMigration/CSVFileHandler.java
+++ src/main/java/com/sparta/employeeDataMigration/CSVFileHandler.java
@@ -1,12 +1,33 @@
 package com.sparta.employeeDataMigration;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class CSVFileHandler {
   public static List<String> readFile() throws FileNotFoundException {
-//    return new ArrayList<String>();
-//      throw new FileNotFoundException();
+
+
+    List<String> result = new ArrayList<>();
+    try (Scanner scanner = new Scanner(new File("employeeDataMigration/src/main/resources/EmployeeRecords.csv"));){
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+        result.add(line);
+      }
+
+      scanner.close();
+      return result;
+
+    } catch (IOException e){
+      e.printStackTrace();
+    }
+
+    return result;
+
   }
+
+
 }
