c430687fc6b17905a315288ebe11fb902851610d
Sahan Seneviratne <sasiniseneviratne@gmail.com>
2023-04-12 20:04:58 +0100

    It didn't update before, but it should now intellij sucks :c
    CSV File handler is still throwing errors in it's readFile() method

--- src/main/java/com/sparta/employeeDataMigration/StringParser.java
+++ src/main/java/com/sparta/employeeDataMigration/StringParser.java
@@ -1,11 +1,42 @@
 package com.sparta.employeeDataMigration;
 
-public class EmployeeParser {
+import java.time.LocalDate;
+import java.util.regex.Pattern;
+
+public class StringParser {
+    
+    
+    public static int parseInt(String epic) throws Exception {
+        String regex = "\\d+";
+        if (epic == null || epic.length() <=0){
+            throw new Exception("Invalid Input");
+
+        }if (Pattern.matches(regex, epic) == false){
+            throw new Exception("Invalid Input");
+        }
+        return Integer.valueOf(epic);
+    }
+
+
+
+
+    public static LocalDate parseDate(String poggers) throws Exception {
+        //04/04/1980
+        String regex = "\\d{2}/\\d{2}/\\d{4}";
+        if(poggers == null || poggers.length() <= 0 || !poggers.matches(regex)){
+            throw new Exception("Invalid date format");
+        }else{
+            String[] segments = poggers.split("/");
+            return LocalDate.of(parseInt(segments[2]), parseInt(segments[1]), parseInt(segments[0]));
+        }
+    }
+    
     //Parser takes employee Array and turns it into an array per row
 
     /**
-     * String[] == {"name, other attribute, salary","name, other attribute, salary"}
-     *
+     * String[] = {"name, other attribute, salary","name, other attribute, salary"}
+     * output
+     * String[] (one employee)= {"name", "other attribute", "salary"}
      *
      * What if we just used this class specifically for parsing from string
      * to employee specific properties using static methods ex:
@@ -21,14 +52,6 @@ public class EmployeeParser {
      * and then we could have a method in the Employee class that creates the employee object
      * and calls these methods to get the right data types from the Strings we pass through.
      */
-    public String[] employeeParser(String[] employeeData){
-        for(String employee : employeeData){
-
-        }
-
-        return employeeData;
-
-    }
 
 
 
